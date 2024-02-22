92cc48f118a2cf8a1aa1295056cc066797505383
dam2 <dam2@Mac-Mini-06.local>
2024-02-22 16:40:48 +0100

    Primer commit, componentes

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
